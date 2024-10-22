0dd41ede2b5fbd2900f57f2069268bd8ec72af4e
aamod0001 <161064214+aamod0001@users.noreply.github.com>
2024-10-22 10:12:41 +0545

    Update rock_paper_siccors.py

--- rock_paper_siccors.py
+++ rock_paper_siccors.py
@@ -1,29 +1,38 @@
 from random import choice
-# rock paper siccors
+
+# Rock Paper Scissors
 
 def game(game_point):
     users_score = 0
     computers_score = 0
-    while game_point != users_score or computers_score:
-        users_choice = input("enter rock(r) papers(p) or siccors(s): ")
+    
+    while users_score < game_point and computers_score < game_point:
+        users_choice = input("Enter rock(r), paper(p), or scissors(s): ").lower()
+        if users_choice not in ['r', 'p', 's']:
+            print("Invalid choice, please try again.")
+            continue
+        
         computers_choice = choice(['r', 'p', 's'])
         if users_choice == computers_choice:
-            print(f"computer has chosen {computers_choice} its a draw")
+            print(f"Computer has chosen {computers_choice}. It's a draw!")
         elif winner(users_choice, computers_choice) == "computer":
-            print(f"computer has chosen {computers_choice} , computer won")
-            computers_score = computers_score + 1
+            print(f"Computer has chosen {computers_choice}. Computer wins!")
+            computers_score += 1
         else:
-            print(f"computer has chosen {computers_choice} , you win")
-            users_score = users_score + 1
-    if users_score == game_point:
-        print("you have won the game")
-    else:
-        print("computer has won the game")
+            print(f"Computer has chosen {computers_choice}. You win!")
+            users_score += 1
 
-def winner(user , computer):
-    if (user == 'p' and computer == 'r') or (user == 's' and computer == 'p') or (user == 'r' and computer == 's'):
-        return "computer"
-    elif (user == 'r' and computer == 'p') or (user == 'p' and computer == 's') or (user == 's' and computer == 'r'):
+    if users_score > computers_score:
+        print("You have won the game!")
+    else:
+        print("Computer has won the game!")
+
+def winner(user, computer):
+    if (user == 'r' and computer == 's') or (user == 'p' and computer == 'r') or (user == 's' and computer == 'p'):
         return "user"
+    elif (user == 's' and computer == 'r') or (user == 'r' and computer == 'p') or (user == 'p' and computer == 's'):
+        return "computer"
+    else:
+        return "draw"
 
 game(1)
